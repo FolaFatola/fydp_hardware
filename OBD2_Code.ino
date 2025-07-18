2f17632cc12b30e2c9d42613ad586aa330a59562
Victoria Angelini <vlangeli@uwaterloo.ca>
2025-07-17 22:59:36 -0400

    speed test

--- OBD2_Code.ino
+++ OBD2_Code.ino
@@ -5,55 +5,53 @@ HardwareSerial OBDSerial(2); // UART2
 #define RX_PIN 16
 #define TX_PIN 17
 
-char rxData[100];
+// buffer to store response data
+char rxData[20];
 uint8_t rxIndex = 0;
+
+int vehicleSpeed = 0;
+int vehicleRPM = 0;
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
-  Serial.println("ESP32 OBD-II Reset Command Test");
 
   // Init UART2 for OBD-II at 9600 baud
   OBDSerial.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
-
   delay(1000);
+
+  Serial.println("ESP32 OBD-II Reset Command Test");
 
   Serial.println("Sending ATZ (reset) command...");
   OBDSerial.println("ATZ");
   delay(2000); // Wait for response
-  OBDSerial.println("ATEO");
-    delay(2000); // Wait for response
-  OBDSerial.println("ATL0");
-    delay(2000); // Wait for response
-  OBDSerial.println("ATS0");
-    delay(2000); // Wait for response
-  OBDSerial.println("ATH0");
-    delay(2000); // Wait for response
-  OBDSerial.println("ATSP0");
 
   getResponse();
   Serial.print("Response: ");
   Serial.println(rxData);
 }
 
-int fromCharToNum(char character) {
-  if (character >= '0' && character <= '9') {
-    return character - '0';
-  } else if (character >= 'A' && character <= 'F') {
-    return character - 'A' + 10;
-  } else if (character >= 'a' && character <= 'f') {
-    return character - 'a' + 10;
-  } else {
-    return -1;
-  }
-}
-
 void loop() {
-  // Nothing here
+  // Get speed
   OBDSerial.println("010D");
-  delay(2000);
+  //Get the response from the OBD-II-UART board. We get two responses
+  //because the OBD-II-UART echoes the command that is sent.
+  //We want the data in the second response.
   getResponse();
-  Serial.println(rxData);
+  getResponse();
+
+  vehicleSpeed = strtol(&rxData[6],0,16);
+  Serial.println("Vehicle speed: " + vehicleSpeed);
+
+  // Get RPM
+  Serial.println("010C");
+  getResponse();
+  getResponse();
+  //Convert the string data to an integer
+  //NOTE: RPM data is two bytes long, and delivered in 1/4 RPM from the OBD-II-UART
+  vehicleRPM = ((strtol(&rxData[6],0,16)*256)+strtol(&rxData[9],0,16))/4;
+
+  delay(100);
 }
 
 // Read response from OBD until '\r' or timeout (3 seconds)
